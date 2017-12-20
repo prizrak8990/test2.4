da8379105c887877c985dd8c313c5b647fe075ab
prizrak8990 <32384718+prizrak8990@users.noreply.github.com>
2017-12-20 20:08:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,79 +4,63 @@ using namespace std;
 
 int main()
 {
-int n,i,j,k,l,t,z;
-cin >> k >> l;
-int **a = new int*[k];
-	for (i=0; i < k; i++){
-        a[i] = new int [l];}
-i=j=z=0;
-t=1;
-for(n=0; n <= k*l ; n++)
-	{
-		if (i==(k-1)/2 && j==(l-1)/2 && k%2==1 && k==l)
-			{
-			 a[i][j]=t++;
-			 continue;
-			}
-		if (i==z && j==l-1-z)
-                               {
-				a[i][j]=t++;
-				i++;
-				continue;
-                                }
-			else
-                                if (i==z)
-					{
-					a[i][j]=t++;
-					j++;
-					continue;
-					}
-			if (i==k-1-z && j==l-1-z)
-                                {
-				a[i][j]=t++;
-				j--;
-				continue;
-                                }
-			else
-                                if (j==l-1-z)
-					{
-					a[i][j]=t++;
-                                        i++;
-					continue;
-                                        }
-			if (i==k-1-z && j==z)
-				{
-				a[i][j]=t++;
-				i--;
-				continue;
-				}
-			else
-                                if (i==k-1-z)
-                                        {
-					a[i][j]=t++;
-					j--;
-					continue;
-                                        }
-			if (i==z+1 && j==z)
-			{
-			a[i][j]=t;
-			z++;
-			continue;
-			}
-			else{
-                                if (j==z)
-					{
-					a[i][j]=t++;
-                                        i--;
-					continue;
-					}
-			}
-			}
-for( i = 0 ; i < k ; i++ )
-{
-	 for( j=0; j < l ; j++)
-		  cout << setw(4) << a[i][j] << " ";
-	cout << endl;
-}
-return 0;
+    int i,j,a,b;
+    cin >> a >> b;
+    int **matrix = new int*[a];
+    for (i=0; i < a; i++){
+        matrix[i] = new int [b];
+    }
+    i=j=0;
+    int t=0;
+    for(int k=1; k < a*b+1; k++) {
+        if (i == t && j!= b - 1 - t) {
+            matrix[i][j] = k;
+            j++;
+            continue;
+        }
+        if (i == t && j == b - 1 - t) {
+            matrix[i][j] = k;
+            i++;
+            continue;
+        }
+        if (j == b - 1 - t && i != a - 1 - t) {
+            matrix[i][j] = k;
+            i++;
+            continue;
+        }
+        if (j == b - 1 - t && i == a - 1 - t) {
+            matrix[i][j] = k;
+            j--;
+            continue;
+        }
+        if (i == a - 1 - t && j != t) {
+            matrix[i][j] = k;
+            j--;
+            continue;
+        }
+        if (i == a - 1 - t && j == t) {
+            matrix[i][j] = k;
+            i--;
+            continue;
+        }
+        if (j == t && i != t + 1) {
+            matrix[i][j] = k;
+            i--;
+            continue;
+        }
+        if (j == t && i == t + 1) {
+            matrix[i][j] = k;
+            j++;
+            t++;
+            continue;
+        }
+    }
+
+    for( i = 0 ; i < a ; i++ )
+    {
+        for( j=0; j < b ; j++)
+            cout << setw(5) <<matrix[i][j] ;
+        cout << endl;
+    }
+    return 0;
 }
